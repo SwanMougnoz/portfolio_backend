26bb660f40ef9356ae1d27aba7e91c1d81a2e370
swan <mougnoz.swan@gmail.com>
2017-07-09 23:21:56 +0200

    List and edit post tags in the /post entry point

--- portfolio_backend/apps/blog/serializers.py
+++ portfolio_backend/apps/blog/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
-from portfolio_backend.apps.blog.models import Post
+from portfolio_backend.apps.blog.models import Post, Tag
 
 
 class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(many=True, slug_field='label', queryset=Tag.objects.all())
+
     class Meta:
         model = Post
         exclude = []
